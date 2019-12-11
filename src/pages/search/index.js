3c8597f66608a6ea49ea03832a0bf2a3585cb65b
liuaaaddxiaoer <oneenddi@163.com>
2019-12-11 21:57:43 +0800

    搜索界面微调

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -77,20 +77,20 @@ class HotSearch extends React.Component {
 
   history() {
 
-    const obj =  (
-      this.state.histories 
-          && (typeof this.state.histories.map === 'function') 
-          && this.state.histories.map((item, index) => {
-            return (
-              <div className={style.history_container} key={index}>
-                <i className="iconfont icon-shijian"></i>
-                <span>{item}</span>
-                <i className="iconfont icon-guanbi"/>
-              </div>
-            )
-          })
+    const obj = (
+      this.state.histories
+      && (typeof this.state.histories.map === 'function')
+      && this.state.histories.map((item, index) => {
+        return (
+          <div className={style.history_container} key={index}>
+            <i className="iconfont icon-shijian"></i>
+            <span>{item}</span>
+            <i className="iconfont icon-guanbi" />
+          </div>
+        )
+      })
     )
-          
+
     console.log(obj)
 
     return obj
@@ -122,7 +122,7 @@ class HotSearch extends React.Component {
 
         {/* 历史记录 */}
         {console.log('history is', typeof this.state.histories.map)}
-        { 
+        {
           this.history()
         }
 
@@ -134,7 +134,7 @@ class HotSearch extends React.Component {
 // eslint-disable-next-line
 class KeywordSearch extends React.Component {
   render() {
-    return(
+    return (
       <div>1</div>
     )
   }
@@ -163,7 +163,7 @@ export default class Search extends React.Component {
     const utils = this.$utils.storage
     let history = utils.getItem('history')
     if (!history) {
-      utils.setItem('history', JSON.stringify([search]))   
+      utils.setItem('history', JSON.stringify([search]))
     } else {
       let histories = JSON.parse(utils.getItem('history'))
       histories.push(search)
@@ -176,11 +176,10 @@ export default class Search extends React.Component {
     const search = e.target.value
     if (!search) return
     if (e.keyCode !== 13) return
-
     // 存储
     // 判断是否有如果有合并
     this.storageHistory(search)
-    
+
     this.setState({
       mode: 3,
       search: search
